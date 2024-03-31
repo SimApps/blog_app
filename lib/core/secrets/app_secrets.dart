7445f8b2a0d513b50f36f88483a0725f68f4a8f1
rami.hamdi <rami.hamdi.2023@gmail.com>
2024-03-31 17:17:15 +0100

    first commit

---
+++ lib/core/secrets/app_secrets.dart
@@ -0,0 +1,5 @@
+class AppSecrets {
+  static const supabaseUrl = 'https://twcmmslaqvbekawutbfx.supabase.co';
+  static const supabaseAnonKey =
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR3Y21tc2xhcXZiZWthd3V0YmZ4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTE4MDc1NTMsImV4cCI6MjAyNzM4MzU1M30.4PJnNJeG3ZxlUQNHaGBJTajIWvsfzzVEkPZhUs9g6oE';
+}
